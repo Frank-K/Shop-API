d8f1cecca13cada708af18b77e609a632ae5d483
Frank Karunaratna <frank.karunaratna@gmail.com>
2019-04-09 11:37:05 -0400

    add db connection and dotenv to the project

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+// mysql db connection
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB
+});
+
+module.exports = pool;
